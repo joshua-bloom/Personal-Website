6ce89b2019e3ff3c64f10a81c751f8277423cc66
Joshua Bloom <joshuabloom@me.com>
2017-06-29 22:37:27 -0400

    fixed

--- send_form_email.php
+++ send_form_email.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_POST['email'])) {
- 
+    
     // EDIT THE 2 LINES BELOW AS REQUIRED
     $email_to = "joshuabloom@me.com";
     $email_subject = "EMAIL SUBJECT HERE";
@@ -13,20 +13,20 @@ if(isset($_POST['email'])) {
         echo "Please go back and fix these errors.<br /><br />";
         die();
     }
- 
+    
     // validation expected data exists
     if(!isset($_POST['first_name']) ||
         !isset($_POST['last_name']) ||
-        !isset($_POST['email']) ||        
+        !isset($_POST['email']) ||
         !isset($_POST['comments'])) {
         died('We are sorry, but there appears to be a problem with the form you submitted.');       
     }
- 
+    
     $first_name = $_POST['first_name']; // required
     $last_name = $_POST['last_name']; // required
     $email_from = $_POST['email']; // required    
     $comments = $_POST['comments']; // required
- 
+    
     $error_message = "";
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
   if(!preg_match($email_exp,$email_from)) {
@@ -46,17 +46,17 @@ if(isset($_POST['email'])) {
     died($error_message);
   }
     $email_message = "Form details below.\n\n";
- 
+    
     function clean_string($string) {
       $bad = array("content-type","bcc:","to:","cc:","href");
       return str_replace($bad,"",$string);
     }
- 
+    
     $email_message .= "First Name: ".clean_string($first_name)."\n";
     $email_message .= "Last Name: ".clean_string($last_name)."\n";
     $email_message .= "Email: ".clean_string($email_from)."\n";   
     $email_message .= "Comments: ".clean_string($comments)."\n";
- 
+    
 // create email headers
 $headers = 'From: '.$email_from."\r\n".
 'Reply-To: '.$email_from."\r\n" .
@@ -65,7 +65,6 @@ $headers = 'From: '.$email_from."\r\n".
 sleep(2);
 echo "<meta http-equiv='refresh' content=\"0; url=http://tutsme-webdesign.info/index.php\">";
 ?>
- 
 <?php
 }
 ?>
